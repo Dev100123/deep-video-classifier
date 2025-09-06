d6a52c30834742e22395eb5d5b1153c042e011c3
gOOdbOy <161236217+Dev100123@users.noreply.github.com>
2025-09-06 19:10:42 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 class CFG:
     classes = ["original", "watermark"] 
-    dataset_path = r"/ibiscostorage/rchandraghosh/no_noice_dataset eps 0.05/training"
+    dataset_path = r"/ibiscostorage/rchandraghosh/video_dataset/dataset with noice eps 0.05/training"
     batch_size = 32
     n_frames = 10
     output_size = (224, 224)
     frame_step = 15
     test_size = 0.2
     random_state = 42
-    epochs = 50
-    model_select = "custom_model"  # options: pre_model
-    model_name = "vgg16"  
+    epochs = 30
+    model_select = "pre_model"  # options: pre_model/custom_model
+    model_name = "resnet50"  # options: vgg16, vgg19, resnet50
     checkpoint_path = "model.h5"
