43378e157bf41d9e01c6aacf2bb0847500a1c5d3
gOOdbOy <rajibdave02@gmail.com>
2025-09-06 03:06:57 +0200

    n

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
 class CFG:
     classes = ["original", "watermark"] 
-    dataset_path = r"C:\Users\RAJIB\Desktop\watermark_video_blockchain\dataset\training"
+    dataset_path = r"C:\Users\RAJIB\Desktop\watermark_video_blockchain\dataset with noice\training"
     batch_size = 32
     n_frames = 10
     output_size = (224, 224)
     frame_step = 15
     test_size = 0.2
     random_state = 42
-    epochs =30
+    epochs = 2
+    model_select = "pre_model"  # options: pre_model
+    model_name = "vgg16"  
     checkpoint_path = "model.h5"
